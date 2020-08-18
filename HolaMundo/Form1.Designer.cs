da0568972c153c7485df2c0fd9f8fd457cd12bd7
Dairo Barrientos <dairobarrientos@gmail.com>
2020-08-18 17:34:31 -0500

    saludo

---
+++ HolaMundo/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace HolaMundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnsaludar = new System.Windows.Forms.Button();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(213, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(332, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mi  Primer Programa";
+            // 
+            // btnsaludar
+            // 
+            this.btnsaludar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsaludar.Location = new System.Drawing.Point(270, 214);
+            this.btnsaludar.Name = "btnsaludar";
+            this.btnsaludar.Size = new System.Drawing.Size(201, 75);
+            this.btnsaludar.TabIndex = 1;
+            this.btnsaludar.Text = "Saludar";
+            this.btnsaludar.UseVisualStyleBackColor = true;
+            this.btnsaludar.Click += new System.EventHandler(this.btnsaludar_Click);
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtnombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnombre.Location = new System.Drawing.Point(247, 123);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(298, 38);
+            this.txtnombre.TabIndex = 2;
+            this.txtnombre.TextChanged += new System.EventHandler(this.txtnombre_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(778, 408);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.btnsaludar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Hola Mundo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnsaludar;
+        private System.Windows.Forms.TextBox txtnombre;
+    }
+}
+
